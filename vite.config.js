4b89c815548fe5a297badc63c83af8746cce6a3a
azharalsharbaji <113019567+azharalsharbaji@users.noreply.github.com>
2023-03-26 11:01:17 +0300

    add build in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -36,6 +36,19 @@ export default defineConfig({
       '.vue',
     ],
   },
+  build: {
+    chunkSizeWarningLimit: 2000,
+    // rollupOptions: {
+    //     output:{
+    //         manualChunks(id) {
+    //           if (id.includes('node_modules')) {
+                
+    //               return id.toString().split('node_modules/')[1].split('/')[0].toString();
+    //           }
+    //       }
+    //     }
+    // }
+  },
   server: {
     port: 3000,
   },
