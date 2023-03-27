f1e5e8262f9530f5a9dab5b2f2f300a4ff61fcbc
azharalsharbaji <113019567+azharalsharbaji@users.noreply.github.com>
2023-03-27 11:15:15 +0300

    adding dist

--- dist/assets/VCardText.1205de4b.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./createSimpleFunctional.206463f8.js";const e=t("v-card-title"),r=t("v-card-text");export{r as V,e as a};
--- dist/assets/cart.6b2955bc.js
+++
@@ -1,1 +0,0 @@
-import{X as e}from"./index.062f8331.js";const r=e("Cart",{persist:!0,state:()=>({productCounter:1,items:[]}),getters:{itemsCount(t){return t.items.length}},actions:{addToCartEarring(t){this.items.push(t)},addToCartNecklace(t){this.items.push(t)},addToCartRing(t){this.items.push(t)},removeFromCart(t){this.items.splice(t,1)},resetCart(){this.items=[]}}});export{r as u};
