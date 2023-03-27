9e5c905759daeec9b626cfad9644446121d7875b
azharalsharbaji <113019567+azharalsharbaji@users.noreply.github.com>
2023-03-27 11:15:15 +0300

    adding dist

---
+++ assets/VCardText.49a172a7.js
@@ -0,0 +1,1 @@
+import{c as t}from"./createSimpleFunctional.c104cba1.js";const e=t("v-card-title"),r=t("v-card-text");export{r as V,e as a};
---
+++ assets/blogs.31b10ed2.js
@@ -0,0 +1,1 @@
+import{X as s}from"./index.4298598e.js";const o=s("blogs",{persist:!0,state:()=>({blogDetails:[]}),getters:{},actions:{ShowBlogDetails(t){this.blogDetails=t}}});export{o as b};
